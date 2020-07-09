80a810c2c0011c007c32f85155d4eda368663b72
Artem <artbreenche@gmail.com>
2020-07-09 19:01:05 +0200

    first

---
+++ reverse string.js
@@ -0,0 +1,10 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+
+function solution(str){
+    //split each letter, reverse, then join
+    return str.split("").reverse().join("");
+  }
+  
+  solution('world');
